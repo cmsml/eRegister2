fd4c73d4f9e4890a02074202d4d4d394aa45ba9c
student <student@WebArch.localdomain>
2012-12-18 18:23:27 +0000

    Session 2: Entered structure into  Instructor and RegClass class definitions

--- grails-app/domain/uk/ac/shu/webarch/eregister2/Instructor.groovy
+++ grails-app/domain/uk/ac/shu/webarch/eregister2/Instructor.groovy
@@ -2,6 +2,11 @@ package uk.ac.shu.webarch.eregister2
 
 class Instructor {
 
+  static mappedBy = [classes: 'instructor']
+
+  static hasMany = [classes: RegClass]
+
+
     static constraints = {
     }
 }
--- grails-app/domain/uk/ac/shu/webarch/eregister2/RegClass.groovy
+++ grails-app/domain/uk/ac/shu/webarch/eregister2/RegClass.groovy
@@ -2,6 +2,8 @@ package uk.ac.shu.webarch.eregister2
 
 class RegClass {
 
+    Instructor    instructor
+
     static constraints = {
     }
 }
